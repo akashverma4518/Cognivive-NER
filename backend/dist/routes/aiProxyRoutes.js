2e5e03c3410d1b43169c42bb3de47102ed8166ca
akashverma451807-glitch <akashverma451807@gmail.com>
2026-09-04 21:01:32 +0530

    chore: remove dependencies and build files from git

--- backend/dist/routes/aiProxyRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const aiProxyController_1 = require("../controllers/aiProxyController");
-const authMiddleware_1 = require("../middleware/authMiddleware");
-const router = (0, express_1.Router)();
-router.post('/voice-parse', authMiddleware_1.authenticateJwt, aiProxyController_1.parseVoiceCommand);
-router.post('/sos-trigger', authMiddleware_1.authenticateJwt, aiProxyController_1.triggerEmergencySos);
-exports.default = router;
--- backend/dist/routes/syncRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const syncController_1 = require("../controllers/syncController");
-const authMiddleware_1 = require("../middleware/authMiddleware");
-const router = (0, express_1.Router)();
-router.post('/batch', authMiddleware_1.authenticateJwt, syncController_1.syncBatch);
-exports.default = router;
